f799d9199d3be869dd9262c767c874d3ee0106d3
Sharmarke <somaliwarsan@gmail.com>
2019-11-07 23:07:13 +0300

    resolving linter issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,33 +1,30 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
-  
   n = arr.size
-  length = arr.size-2
-  
+  length = arr.size - 2
+
   n.times do
     0.upto(length) do |i|
-        if (arr[i] > arr[i+1] )
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-        end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
     end
   end
-  return arr
+  arr
 end
 
 def bubble_sort_by(arr)
-    n = arr.size
-    length = arr.size-2
-    
-    n.times do
-      0.upto(length) do |i|
-        sort = yield(arr[i], arr[i+1])
-        if sort > 0
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-        end
-      end
+  n = arr.size
+  length = arr.size - 2
+
+  n.times do
+    0.upto(length) do |i|
+      sort = yield(arr[i], arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if sort > 0
     end
-    return arr
+  end
+  arr
 end
 
-puts bubble_sort_by([33,56,1,90,66]) { |left, right|
+puts bubble_sort_by([33, 56, 1, 90, 66]) { |left, right|
   left - right
 }
